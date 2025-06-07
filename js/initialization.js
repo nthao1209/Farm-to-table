41360d4c1355d1327749a7a9b0f229ae46785c33
nthao1209 <163665504+nthao1209@users.noreply.github.com>
2025-06-07 17:31:31 +0700

    Initial commit

---
+++ js/initialization.js
@@ -0,0 +1,22 @@
+// Create admin account 
+function createAdminAccount() {
+    let accounts = localStorage.getItem("accounts");
+    if (!accounts) {
+        accounts = [];
+        accounts.push({
+            fullname: "Trần Ngọc Chi",
+            phone: "0123456789",
+            password: "123456",
+            address: '',
+            email: '',
+            status: 1,
+            join: new Date(),
+            cart: [],
+            userType: 1
+        })
+        localStorage.setItem('accounts', JSON.stringify(accounts));
+    }
+}
+
+window.onload = createProduct();
+window.onload = createAdminAccount();
